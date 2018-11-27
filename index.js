490b1c7df2392015fa263e6a9c65809aad0e28e4
Kate Drennen <k8ted11@gmail.com>
2018-11-27 03:07:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === true) {
+  if (whisper.toLowerCase() === true) {
     return "I can`'t hear you!"
   }
 }

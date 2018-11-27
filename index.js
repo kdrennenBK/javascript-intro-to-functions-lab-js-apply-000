83e5b8942b9bed18de7e758a90bc8f14b84ee1c3
Kate Drennen <k8ted11@gmail.com>
2018-11-27 03:28:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string === "hello!") {
+  var lowercase = "hello!"
+  var uppercase = "HELLO!"
+  if (string === lowercase) {
     return "I can\'t hear you!";
-  } else if (string === "HELLO!" ) {
+  } else if (string === uppercase) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
